eb1c48e75ecf6c0fb30e352467168d754433bf00
Kundan Tripathi <kundantripathi1512@gmail.com>
2024-06-06 20:34:10 +0530

    updated

---
+++ src/components/Menu.jsx
@@ -0,0 +1,45 @@
+import Pizza from "./Pizza";
+
+function Menu({pizzaData}) {
+    const pizzas = pizzaData;
+    // const pizzas = [];
+    const numPizzas = pizzas.length;
+  
+    return (
+      <main className="menu">
+        <h2>Our menu</h2>
+  
+        {numPizzas > 0 ? (
+          <>
+            <p>
+              Authentic Italian cuisine. 6 creative dishes to choose from. All
+              from our stone oven, all organic, all delicious.
+            </p>
+  
+            <ul className="pizzas">
+              {pizzas.map((pizza) => (
+                <Pizza pizzaObj={pizza} key={pizza.name} />
+              ))}
+            </ul>
+          </>
+        ) : (
+          <p>{"We're still working on our menu. Please come back later :)"}</p>
+        )}
+  
+        {/* <Pizza
+          name="Pizza Spinaci"
+          ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+          photoName="pizzas/spinaci.jpg"
+          price={10}
+        />
+        <Pizza
+          name="Pizza Funghi"
+          ingredients="Tomato, mushrooms"
+          price={12}
+          photoName="pizzas/funghi.jpg"
+        /> */}
+      </main>
+    );
+}
+
+export default Menu;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Header from "./components/Header";
+import Menu from "./components/Menu";
 
 const pizzaData = [
   {
@@ -52,7 +53,7 @@ function App() {
   return (
     <div className="container"> 
       <Header />
-      <Menu />
+      <Menu pizzaData={pizzaData}/>
       <Footer />
       <Order/>
     </div>
@@ -60,71 +61,6 @@ function App() {
 }
 
 
-function Menu() {
-  const pizzas = pizzaData;
-  // const pizzas = [];
-  const numPizzas = pizzas.length;
-
-  return (
-    <main className="menu">
-      <h2>Our menu</h2>
-
-      {numPizzas > 0 ? (
-        <>
-          <p>
-            Authentic Italian cuisine. 6 creative dishes to choose from. All
-            from our stone oven, all organic, all delicious.
-          </p>
-
-          <ul className="pizzas">
-            {pizzas.map((pizza) => (
-              <Pizza pizzaObj={pizza} key={pizza.name} />
-            ))}
-          </ul>
-        </>
-      ) : (
-        <p>{"We're still working on our menu. Please come back later :)"}</p>
-      )}
-
-      {/* <Pizza
-        name="Pizza Spinaci"
-        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-        photoName="pizzas/spinaci.jpg"
-        price={10}
-      />
-      <Pizza
-        name="Pizza Funghi"
-        ingredients="Tomato, mushrooms"
-        price={12}
-        photoName="pizzas/funghi.jpg"
-      /> */}
-    </main>
-  );
-}
-
-function Pizza({ pizzaObj }) {
-  console.log(pizzaObj);
-
-  // if (pizzaObj.soldOut) return null;
-
-  return (
-    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
-      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
-      <div>
-        <h3>{pizzaObj.name}</h3>
-        <p>{pizzaObj.ingredients}</p>
-
-        {/* {pizzaObj.soldOut ? (
-          <span>SOLD OUT</span>
-        ) : (
-          <span>{pizzaObj.price}</span>
-        )} */}
-
-        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
-      </div>
-    </li>
-  );
-}
 
 function Footer() {
   const hour = new Date().getHours();
